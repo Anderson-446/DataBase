b5425e5712f484a8f0ee409e051b4669253e907f
Anderson-446 <emanoelanderson16@gmail.com>
2023-05-09 22:11:38 -0300

    ;

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
-CREATE SCHEMA `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA `database` DEFAULT CHARACTER SET utf8 ;
+USE `database` ;
 
 
-CREATE TABLE `mydb`.`USERS` (
+CREATE TABLE `database`.`USERS` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `user_username` VARCHAR(45) NOT NULL,
   `user_email` VARCHAR(100) NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE `mydb`.`USERS` (
   `type` ENUM('adm', 'com') NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE INDEX `email_UNIQUE` (`user_email` ASC) VISIBLE
-  )
+  );
 
 
-CREATE TABLE `mydb`.`POSTS` (
+CREATE TABLE `database`.`POSTS` (
   `post_id` INT NOT NULL AUTO_INCREMENT, 
   `post_titulo` VARCHAR(255) NOT NULL,
   `post_conteudo` MEDIUMTEXT NOT NULL,
@@ -22,14 +22,14 @@ CREATE TABLE `mydb`.`POSTS` (
   PRIMARY KEY (`post_id`),
   INDEX `fk_POSTS_USERS1_idx` (`USERS_user_id` ASC) VISIBLE,
   CONSTRAINT `fk_POSTS_USERS1`
-    FOREIGN KEY (`USERS_user_id`) --referencia a tabela, depois a coluna
-    REFERENCES `mydb`.`USERS` (`user_id`) --esquema . tabela . chave primária--
+    FOREIGN KEY (`USERS_user_id`)
+    REFERENCES `database`.`USERS` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
-    )
+    );
 
 
-CREATE TABLE `mydb`.`COMMENTS` (
+CREATE TABLE `database`.`COMMENTS` (
   `comment_id` INT NOT NULL AUTO_INCREMENT,
   `comment_text` TEXT(500) NOT NULL,
   `comment_data` DATETIME(6) NOT NULL,
@@ -40,25 +40,25 @@ CREATE TABLE `mydb`.`COMMENTS` (
   INDEX `fk_COMMENTS_POSTS1_idx` (`POSTS_post_id` ASC) VISIBLE,
   CONSTRAINT `fk_COMMENTS_USERS1` -- foreing key + tabela primary + tabela foreign
     FOREIGN KEY (`USERS_user_id`)
-    REFERENCES `mydb`.`USERS` (`user_id`)
+    REFERENCES `database`.`USERS` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_COMMENTS_POSTS1`
     FOREIGN KEY (`POSTS_post_id`)
-    REFERENCES `mydb`.`POSTS` (`post_id`)
+    REFERENCES `database`.`POSTS` (`post_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
-    )
+    );
 
 
-CREATE TABLE `mydb`.`TAGS` (
+CREATE TABLE `database`.`TAGS` (
   `tag_id` INT NOT NULL AUTO_INCREMENT,
   `tag_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`tag_id`)
-  )
+  );
 
 
-CREATE TABLE `mydb`.`TAGGING` (
+CREATE TABLE `database`.`TAGGING` (
   `POSTS_post_id` INT NOT NULL,
   `TAGS_tag_id` INT NOT NULL,
   PRIMARY KEY (`POSTS_post_id`, `TAGS_tag_id`),
@@ -66,15 +66,15 @@ CREATE TABLE `mydb`.`TAGGING` (
   INDEX `fk_POSTS_has_TAGS_POSTS1_idx` (`POSTS_post_id` ASC) VISIBLE,
   CONSTRAINT `fk_POSTS_has_TAGS_POSTS1`
     FOREIGN KEY (`POSTS_post_id`)
-    REFERENCES `mydb`.`POSTS` (`post_id`)
+    REFERENCES `database`.`POSTS` (`post_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_POSTS_has_TAGS_TAGS1`
     FOREIGN KEY (`TAGS_tag_id`)
-    REFERENCES `mydb`.`TAGS` (`tag_id`)
+    REFERENCES `database`.`TAGS` (`tag_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
-    )
+    );
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
